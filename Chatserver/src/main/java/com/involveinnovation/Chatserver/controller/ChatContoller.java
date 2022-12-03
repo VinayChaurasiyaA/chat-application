5f3163b70bc8d0aff3ff8b58456b3717210a9ca4
VINAY CHAURASIYA <92705023+VinayChaurasiyaA@users.noreply.github.com>
2022-12-03 22:22:09 +0530

    Update ChatContoller.java

--- Chatserver/src/main/java/com/involveinnovation/Chatserver/controller/ChatContoller.java
+++ Chatserver/src/main/java/com/involveinnovation/Chatserver/controller/ChatContoller.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatContoller {
+    @Autowired
     private SimpMessagingTemplate simpMessagingTemplate; // this will help us for the dynamic creation of send to
 
     @MessageMapping("/message") // This is for the public message path
@@ -21,6 +22,7 @@ public class ChatContoller {
     public Message receivePrivateMessage(@Payload Message message) {
         // here we are not using @SendTo, so instead we'll use dynamic send to:
         simpMessagingTemplate.convertAndSendToUser(message.getReceiverName() , "/private" , message); // it will directly take the user destination which we set in config file
+        System.out.println(message.toString());
         return message;
     }
 }
